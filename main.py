c4d09cae169cc66bda7ba1f9b68827165cc4f768
Louis Goffin <louisgoffin90@gmail.com>
2026-02-01 16:30:12 +0100

    Proper recursive iteration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from pathlib import Path
 from moviepy import VideoFileClip
 import shutil
@@ -16,6 +17,12 @@ def get_video_orientation(video_path):
     except Exception as e:
         print(f"Error processing {video_path}: {e}")
         return None
+
+def get_recursive_folders(folder: Path, excluded_names: list[str], deep_recursive: bool = False) -> list[Path]:
+    if deep_recursive:
+        return [folder] + [Path(r) for r, d, files in os.walk(folder) if r not in excluded_names]
+    else:
+        return [folder] + [Path(f) for f in os.scandir(folder) if f.is_dir() and f.name not in excluded_names]
 
 def sort_videos_by_orientation(folder):
     folder = Path(folder)
@@ -43,16 +50,18 @@ if __name__ == "__main__":
         prog="FlipTastic",
         description="Sort video files according to their orientation."
     )
-    parser.add_argument("-r", "--recursive", action="store_true", help="Look recursively for subfolders.")
-    parser.add_argument("-f", "--folder", type=str, required=True, help="Folder in which to look for videos.")
+    parser.add_argument("-r", "--recursive", action="store_true", help="look recursively for subfolders")
+    parser.add_argument("-f", "--folder", type=str, required=True, help="folder in which to look for videos")
     args = parser.parse_args()
     if args.recursive:
+        # list all folders to process
+        subfolders = get_recursive_folders(args.folder, excluded_names=["..", "portrait", "landscape"], deep_recursive=True)
+        print(f"Found {len(subfolders)} subfolders to process in {args.folder}.")
         # loop over subfolders
-        for subfolder in Path(args.folder).iterdir():
-            if subfolder.is_dir() and subfolder.name not in ['portrait', 'landscape', '..']:
-                print(f"Processing folder: {subfolder} ...")
-                sort_videos_by_orientation(subfolder)
-                print(f"Done sorting videos in {subfolder} !")
+        for subfolder in subfolders:
+            print(f"Processing folder: {subfolder} ...")
+            sort_videos_by_orientation(subfolder)
+            print(f"Done sorting videos in {subfolder} !")
     else:
         # process only current folder
         print(f"Processing folder: {args.folder} ...")
